f56dcb61612353cd3235a461cbb293b30443779a
Heri Risnanto <sparta.nibel@gmail.com>
2022-10-14 20:48:08 +0700

    chore: add & setup histoire

--- src/components.d.ts
+++ src/components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Counter: typeof import('./components/Counter.vue')['default']
+    'Counter.story': typeof import('./components/Counter.story.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     README: typeof import('./components/README.md')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
---
+++ src/components/Counter.story.vue
@@ -0,0 +1,5 @@
+<template>
+    <Story>
+        <Counter :initial="12" />
+    </Story>
+</template>
